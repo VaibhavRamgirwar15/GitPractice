49e95dd4adec2c42df1ea69ec5cca923aff7c08a
Vaibhav Ramgirwar <60150231+VaibhavRamgirwar15@users.noreply.github.com>
2020-03-24 15:21:33 +0530

    updated HelloWorld java file
    - added comments to line 
    - added new sysout line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@ Class GitPracticeJavaCode{
 
   public static void main(args [])
   {
+    //adding first line
     System.out.println("Hello World");
+    //added second line
+    System.out.println("Welcome to the New GitHub World");
   }
 }
